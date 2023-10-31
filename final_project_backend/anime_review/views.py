722005fc0424ff643e4a11f3fb492cc50c81a09f
Brandon D McAbee <bdmm18@gmail.com>
2023-10-30 22:55:45 -0400

    add some viewsets..they need work

--- final_project_backend/anime_review/views.py
+++ final_project_backend/anime_review/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from anime_review.models import UserProfile, Shows, Reviews
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    shows = Shows.objects.all()
+    return render(request, 'index.html', {'shows': shows})
+
+def show_details(request, show_id):
+    show = Shows.objects.get(id=show_id)
+    reviews = Reviews.objects.filter(show=show)
+    return render(request, 'show_detail.html', {'show': show, 'reviews':reviews})
+
+
+def user_profile(request, user_id):
+    user_profile = UserProfile.objects.get(id=user_id)
+    return render(request, 'user_profile.html', {'user_profile': user_profile})
