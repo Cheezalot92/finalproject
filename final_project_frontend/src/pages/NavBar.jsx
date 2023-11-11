fb66c0c95f6c80ba411ae5ca49805d202144de31
Brandon D McAbee <bdmm18@gmail.com>
2023-11-11 11:13:10 -0500

    Fixed the css on my userprofile and welcome page , add more

--- final_project_frontend/src/pages/NavBar.jsx
+++ final_project_frontend/src/pages/NavBar.jsx
@@ -1,6 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { StyledNav } from "../components/NavBarCss";
+import styled from "styled-components";
+
+
+
+  const StyledNav = styled.nav`
+  background-color: #060606;
+  padding: 15px;
+  text-align: center;
+
+  a {
+    color: #f5f5f5;
+    text-decoration: none;
+    padding: 10px;
+    display: inline-block;
+
+    &:hover {
+      background-color: #f70404;
+      color: #121111;
+    }
+  }
+
+  @media (max-width: 768px) {
+    padding: 5px;
+
+    a {
+      padding: 5px;
+    }
+  }
+`;
 
 const NavBar = () => {
   return (
--- final_project_frontend/src/pages/WelcomePage.jsx
+++ final_project_frontend/src/pages/WelcomePage.jsx
@@ -3,6 +3,64 @@ import { StyleWelcome } from "../components/WelcomePageCss";
 import NavBar from "./NavBar";
 
 
+
+
+const StyleWelcome = styled.div`
+
+
+.container {
+    width: 80%;
+    margin: 0 auto;
+    text-align: center;
+  }
+  
+  h1 {
+    outline: 5px outset white;
+    outline-color: #f7f7f7;
+    color: red;
+    font-size: 40px;
+    margin-bottom: 20px;
+  }
+  
+  p {
+    color: red;
+    font-size: 18px;
+    margin-bottom: 40px;
+  }
+  
+  .boxes {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+  
+  .box {
+    width: 270px;
+    height: 270px;
+    margin: 20px;
+    text-align: center;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    padding: 20px;
+  }
+  
+  .box img {
+    width: 220px;
+    height: 120px;
+    margin: 0 auto;
+    display: block;
+
+  }
+  
+  h2 {
+    font-size: 20px;
+    margin-top: 20px;
+    margin-bottom: 10px;
+  }
+  `
+
+  
+
 export default function WelcomePage(props) {
     return (
         <StyleWelcome>
